4e773477b4debbe42929b0ba1a0f14513015d205
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-09 09:29:37 +0530

    refactoring joke creation logic to be at utility class

--- src/main/java/com/behl/glumon/controller/JokeController.java
+++ src/main/java/com/behl/glumon/controller/JokeController.java
@@ -1,7 +1,5 @@
 package com.behl.glumon.controller;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.behl.glumon.utility.JokeUtility;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @AllArgsConstructor
-@Slf4j
 public class JokeController {
 
     @GetMapping(value = "/joke", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -28,14 +23,7 @@ public class JokeController {
     @Operation(summary = "Returns a funny joke")
     public ResponseEntity<?> jokeRetreivalHandler(
             @Parameter(hidden = true) @RequestHeader(name = "Authorization", required = true) final String header) {
-        final var response = new JSONObject();
-        try {
-            response.put("joke", JokeUtility.generate());
-        } catch (JSONException e) {
-            log.error("Unable to generate JSON response");
-            throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
-        }
-        return ResponseEntity.ok(response.toString());
+        return ResponseEntity.ok(JokeUtility.generate());
     }
 
 }
