776c8c2966b3146023b0638111d688e267923da6
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-09 07:11:28 +0530

    init project commit

---
+++ src/main/java/com/behl/glumon/RateLimitingApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.behl.glumon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitingApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitingApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behl/glumon/RateLimitingApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behl.glumon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitingApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
