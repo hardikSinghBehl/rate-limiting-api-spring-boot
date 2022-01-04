895e3671f26978be790808947fbeefd2e825249d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-01-04 14:52:42 +0530

    adding flyway

---
+++ src/main/resources/db/migration/V001__adding_initial_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `plans` (
+  `id` binary(32) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `limit_per_hour` int NOT NULL UNIQUE,
+  `name` varchar(255) NOT NULL UNIQUE,
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` binary(32) NOT NULL,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `email_id` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_plan_mappings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `is_active` bit(1) NOT NULL,
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `plan_id` binary(32) NOT NULL,
+  `user_id` binary(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrxbu9b6ar3bmufhyaq8t93ve5` (`plan_id`),
+  KEY `FKde26wx3e5d2x2j1fbn8m2g5t8` (`user_id`),
+  CONSTRAINT `FKde26wx3e5d2x2j1fbn8m2g5t8` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKrxbu9b6ar3bmufhyaq8t93ve5` FOREIGN KEY (`plan_id`) REFERENCES `plans` (`id`)
+);
---
+++ src/main/resources/db/migration/V002__configuring_plans.sql
@@ -0,0 +1,3 @@
+INSERT INTO plans (id, limit_per_hour, name) VALUES (UUID_TO_BIN('631d899b-baf5-46ca-891f-0b36483ba26f'), 20, 'FREE');
+INSERT INTO plans (id, limit_per_hour, name) VALUES (UUID_TO_BIN('519b0e8f-911c-4a5d-8b3f-eaac88c3d5fb'), 40, 'BUSINESS');
+INSERT INTO plans (id, limit_per_hour, name) VALUES (UUID_TO_BIN('a1654488-2d82-497b-8c0b-ed8b97d4a3e2'), 100, 'PROFESSIONAL');
